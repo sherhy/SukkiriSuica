ccb61469de0637a632efb2b45069e1e1e6bc6b58
MasakiKomemoto <komemoto1997@gmail.com>
2018-09-11 18:39:57 +0900

    firstcommit

--- app/Controller/top/index.php
+++ app/Controller/top/index.php
@@ -1,14 +1,9 @@
 <?php
-
 use Slim\Http\Request;
 use Slim\Http\Response;
-
 // TOPページのコントローラ
 $app->get('/', function (Request $request, Response $response) {
-
     $data = [];
-
     // Render index view
     return $this->view->render($response, 'top/index.twig', $data);
 });
-
