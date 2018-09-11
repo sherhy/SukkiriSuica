2578129577634665664ce2ea2c000bb9de289902
hyunjin <hyunjin.jeong@toki.waseda.jp>
2018-09-11 17:28:04 +0900

    routing

--- app/Controller/sukkiri/index.php
+++ app/Controller/sukkiri/index.php
@@ -3,14 +3,97 @@
 use Slim\Http\Request;
 use Slim\Http\Response;
 use Model\Dao\User;
+use Model\Dao\Item;
+
+function pat($a, $l) {
+  $x = array('');
+  for ($i = 0; $i < $l; ++$i) {
+    $_x = array();
+    foreach ($a as $ak => $av) {
+      $j=0;
+	    foreach ($x as $xk => $xv) {
+        $xv .= $av;
+          if($j<count($x))
+            $xv .= ',';
+          $_x[] = $xv;
+          $j=$j+1;
+        }
+      }
+      $x = $_x;
+    }
+	return $x;
+}
+
+
+		// $data = [];
+  //   $userItem = new Item($this->db);
+  //   $where = [
+  //       'price' => 100,
+  //       'name'  => '%チョコレート%'
+  //   ];
+  //   $data = $userItem->select($where);
+  //   var_dump($data);
 
 // スッキリ画面コントローラ
-$app->get('/results/', function (Request $request, Response $response) {
+$app->post('/results/', function (Request $request, Response $response) {
 
-    //GETされた内容を取得します。
-    $data = $request->getQueryParams();
+	//GETされた内容を取得します。
+	$limit = $request->getParsedBody();
+	$minarray = array();
+  $min=100;
 
-    // Render index view
-    return $this->view->render($response, 'sukkiri/sukkiri.twig', $data);
+  $userItem = new Item($this->db);
+  $a = $userItem->distinct();
+  var_dump($a);
+  $a = array('11','32','54','108','216','324');
+  $l = 4;
+  $i=1;
+  while($i<=$l)
+  {
+    $x=array();
+    $x=pat($a,$i);
+    foreach($x as $xy)
+    {
+      $xy=substr($xy,0,-1);
+      $arr = explode(',', $xy);
+      if(array_sum($arr)==$limit)
+      {
+        $min=0;
+        $minarray=$arr;
+        break;
+      }
+      elseif(array_sum($arr)<$limit)
+      {
+        $minx=$limit-array_sum($arr);
+        if($min>$minx)
+        {
+          $min=$minx;
+          $minarray=$arr;
+        }
+      }
+    }
+    if($min==0)
+    {
+      break;
+    }
+    $i += 1;
+  }
+
+
+
+	// $res = [88, 206]
+
+  // Render index view
+	return $this->view->render($response, 'sukkiri/sukkiri.twig', $res);
 
 });
+
+// [{
+// 	"name": asdvasdv,
+// 	"price": 324,
+// 	"img": "www.family.co.jp/lsdf"
+// },{
+// 	"name": asdflkj,
+// 	"price": 54,
+// 	"img": 'www.family.co.jp/adsl;fkjasd'
+// }]
