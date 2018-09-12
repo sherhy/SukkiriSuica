7f64340ecb7a200a88ffd79ab77ab08532fd64dc
hyunjin <hyunjin.jeong@toki.waseda.jp>
2018-09-12 11:45:39 +0900

    syntax fix and in-tax price

--- app/Controller/sukkiri/index.php
+++ app/Controller/sukkiri/index.php
@@ -6,21 +6,21 @@ use Model\Dao\User;
 use Model\Dao\Item;
 
 function pat($a, $l) {
-  $x = array('');
-  for ($i = 0; $i < $l; ++$i) {
-    $_x = array();
-    foreach ($a as $ak => $av) {
-      $j=0;
-	    foreach ($x as $xk => $xv) {
-        $xv .= $av;
-          if($j<count($x))
-            $xv .= ',';
-          $_x[] = $xv;
-          $j=$j+1;
-        }
-      }
-      $x = $_x;
-    }
+	$x = array('');
+	for ($i = 0; $i < $l; ++$i) {
+		$_x = array();
+		foreach ($a as $ak => $av) {
+			$j=0;
+			foreach ($x as $xk => $xv) {
+				$xv .= $av;
+					if($j<count($x))
+						$xv .= ',';
+					$_x[] = $xv;
+					$j=$j+1;
+				}
+			}
+			$x = $_x;
+		}
 	return $x;
 }
 
@@ -34,54 +34,54 @@ $app->post('/results/', function (Request $request, Response $response) {
 	// var_dump($limit);
 
 	$minarray = array();
-  $min=100;
+	$min=100;
 
-  $userItem = new Item($this->db);
-  $c = $userItem->distinct();
-  $a = array();
-  foreach($c as $b){
+	$userItem = new Item($this->db);
+	$c = $userItem->distinct();
+	$a = array();
+	foreach($c as $b){
 		array_push($a, strval($b["price"]));
-  }
-  // $a = array('11','32','54','108','216','324');
-  $l = 4;
-  $i=1;
-  while($i<=$l)
-  {
-    $x=array();
-    $x=pat($a,$i);
-    foreach($x as $xy)
-    {
-      $xy=substr($xy,0,-1);
-      $arr = explode(',', $xy);
-      if(array_sum($arr)==$limit){
-        $min=0;
-        $minarray=$arr;
-        break;
-      } elseif(array_sum($arr)<$limit) {
-        $minx=$limit-array_sum($arr);
-        if($min>$minx) {
-          $min=$minx;
-          $minarray=$arr;
-        }
-      }
-    }
-    if($min==0)
-    {
-      break;
-    }
-    $i += 1;
-  }
-  $res = array();
-  $total = 0;
-  foreach ($minarray as $_) {
-  	$param["price"]=$_;
-  	$result=$userItem->select($param, "", "", 1, false);
-  	$total += $result['price'];
-  	array_push($res, $result);
-  }
-  // var_dump($res);
+	}
+	// $a = array('11','32','54','108','216','324');
+	$l = 4;
+	$i=1;
+	while($i<=$l)
+	{
+		$x=array();
+		$x=pat($a,$i);
+		foreach($x as $xy)
+		{
+			$xy=substr($xy,0,-1);
+			$arr = explode(',', $xy);
+			if(array_sum($arr)==$limit){
+				$min=0;
+				$minarray=$arr;
+				break;
+			} elseif(array_sum($arr)<$limit) {
+				$minx=$limit-array_sum($arr);
+				if($min>$minx) {
+					$min=$minx;
+					$minarray=$arr;
+				}
+			}
+		}
+		if($min==0)
+		{
+			break;
+		}
+		$i += 1;
+	}
+	$res = array();
+	$total = 0;
+	foreach ($minarray as $_) {
+		$param["price"]=$_;
+		$result=$userItem->select($param, "", "", 1, false);
+		$total += $result['price'];
+		array_push($res, $result);
+	}
+	// var_dump($res);
 
-  // Render index view
+	// Render index view
 	return $this->view->render($response, 'sukkiri/sukkiri.twig', [
 		'res' => $res,
 		'total' => $total
