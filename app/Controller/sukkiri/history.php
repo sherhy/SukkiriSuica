092fcbd23623d8fec173f010c1f1cdb049e33314
秋元尚耶 <akitkat.cook.dx@gmail.com>
2018-09-12 16:10:10 +0900

    feat:ログ取得ロジックできた。

--- app/Controller/sukkiri/history.php
+++ app/Controller/sukkiri/history.php
@@ -2,6 +2,8 @@
 
 use Slim\Http\Request;
 use Slim\Http\Response;
+use Model\Dao\SukkiriLog;
+use Model\Dao\Item;
 
 // スッキリログコントローラ
 $app->get('/history/', function (Request $request, Response $response) {
@@ -9,6 +11,16 @@ $app->get('/history/', function (Request $request, Response $response) {
     //GETされた内容を取得します。
     $data = $request->getQueryParams();
 
+//     var_dump($data);
+
+    // 対象ログ取得
+    $sukkiriLogQuery = new SukkiriLog($this->db);
+    $logs = $sukkiriLogQuery->select(['log_id' => $data['log_id']],null, null, 1);
+
+    // 商品取得
+    $itemQuery = new Item($this->db);
+    $items = $itemQuery->getItems($logs['ids']);
+
 	// Render index view
-	return $this->view->render($response, 'sukkiri/history.twig', $data);
+	return $this->view->render($response, 'sukkiri/history.twig', ['res' => $items]);
 });
--- app/Model/Dao/Item.php
+++ app/Model/Dao/Item.php
@@ -1,8 +1,27 @@
 <?php
 
 namespace Model\Dao;
+
 class Item extends Dao
 {
-	
+
+    /**
+     * itemのidでin句で全部取る。
+     *
+     * @param string $item_ids
+     * @return array
+     */
+    public function getItems(string $item_ids):array
+    {
+        $sql = "
+            select
+            	*
+            from
+            	item
+            where
+            	id in ($item_ids)
+        ";
+        return $this->db->fetchAll($sql);
+    }
 
 }
