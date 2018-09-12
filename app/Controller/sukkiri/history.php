3ab9a9a3c8e14c92a091b58d15fc0c887e1f8bfc
秋元尚耶 <akitkat.cook.dx@gmail.com>
2018-09-12 15:25:46 +0900

    feat:ログページ作成。

---
+++ app/Controller/sukkiri/history.php
@@ -0,0 +1,14 @@
+<?php
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+// スッキリログコントローラ
+$app->post('/history/', function (Request $request, Response $response) {
+
+    //GETされた内容を取得します。
+    $data = $request->getQueryParams();
+
+	// Render index view
+	return $this->view->render($response, 'sukkiri/history.twig', $data);
+});
